31225171ee24da61642c105fdebd6d4b5b281947
Victor Campos <victor.campos@bigcommerce.com>
2025-03-21 15:54:05 -0600

    fix: custom buyer portal url

--- core/b2b/scripts.tsx
+++ core/b2b/scripts.tsx
@@ -15,7 +15,7 @@ interface Props {
 export function B2BProductionScripts({ storeHash, channelId, token, environment }: Props) {
   useB2BAuth(token);
   const customBuyerPortalHost = process.env.CUSTOM_BUYER_PORTAL_HOST;
-
+  const src = customBuyerPortalHost ? `${customBuyerPortalHost}/headless.js` : `https://cdn.bundleb2b.net/b2b/${environment}/storefront/headless.js`;
   return (
     <>
       <Script id="b2b-config">
@@ -34,7 +34,7 @@ export function B2BProductionScripts({ storeHash, channelId, token, environment 
         data-channelid={channelId}
         data-storehash={storeHash}
         data-environment={environment}
-        src={`${customBuyerPortalHost}/headless.js` || `https://cdn.bundleb2b.net/b2b/${environment}/storefront/headless.js`}
+        src={src}
         type="module"
       />
     </>
